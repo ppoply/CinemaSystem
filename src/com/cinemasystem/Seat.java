657e8a7e6d48e243a314d8d514743a99a52ba3e0
Kelvin1337 <30276824+Kelvin1337@users.noreply.github.com>
2017-12-06 17:54:47 +0400

    Seat file
    1. No requirement for type and vip booleans. Can check if a seat reference variable is vip using the instance of operator
    
    2. Prices arent final. Hanled with an abstract method

--- src/com/cinemasystem/Seat.java
+++ src/com/cinemasystem/Seat.java
@@ -1,52 +1,50 @@
 package com.cinemasystem;
 
-public class Seat
+public abstract class Seat
 {
     protected char row;
     protected int col;
-    protected String type;
-    protected boolean occupied, vip, regular;
+    protected boolean occupied;
 
 
     public Seat(char r, int c)
     {
         row = r;
-        col =c;
+        col = c;
         occupied = false;
     }
 
     public String toString()
     {
-        return type+"/nRow:"+row+"/nSeat Number:"+col;
+        return "Seat: "+row + col;
     }
 
-    public static void main(String[] args)
-    {
-        
-    }
+    public abstract double getPrice()
 }
 
 class EconomySeat extends Seat
 {
-    final protected int price = 35;
     public EconomySeat(char r, int c)
     {
         super(r, c);
-        type = "Economy";
-        vip = false;
-        regular = true;
+    }
+
+    public double getPrice()
+    {
+        return 35;                  //Should be handled by file
     }
 }
 
 class VIPSeat extends Seat
 {
-    final protected int price = 55;
     public VIPSeat(char r, int c)
     {
         super(r,c);
-        type= "VIP";
-        vip = true;
-        regular = false;
+    }
+
+    public double getPrice()
+    {
+        return 55;                  //Should be handled by file
     }
 }
 
